246bd281f0df688fcc5b0140392407b58203cb97
b7av3.t7av3ler <101125385+wh1ter0seunm4skedX@users.noreply.github.com>
2024-01-31 15:32:16 +0200

    0.2

---
+++ src/test/java/com/b7av3/loginapp/LoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b7av3.loginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
