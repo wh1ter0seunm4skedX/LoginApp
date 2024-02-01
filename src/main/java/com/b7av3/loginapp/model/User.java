54b4810b5855f69ec96440f1b7e488b734bfc2ae
b7av3.t7av3ler <101125385+wh1ter0seunm4skedX@users.noreply.github.com>
2024-02-01 21:22:17 +0200

    Comments to User.java
    Added comments to all methods for better understanding for the future.

--- src/main/java/com/b7av3/loginapp/model/User.java
+++ src/main/java/com/b7av3/loginapp/model/User.java
@@ -1,52 +1,66 @@
 package com.b7av3.loginapp.model;
-import javax.validation.constraints.Size;
 
+import javax.validation.constraints.Size;
 import jakarta.persistence.*;
+
+// This class represents a User entity in the application.
 @Entity
 public class User {
+
+    // The primary key for the User entity
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // The primary key
+    private Long id;
 
+    // User's username, must be between 4 and 15 characters in length
     @Column(nullable = false, unique = true)
     @Size(min = 4, max = 15)
     private String username;
 
+    // User's password, must be at least 8 characters in length
     @Column(nullable = false)
     @Size(min = 8)
     private String password;
 
+    // Default constructor for User
     public User() {
     }
 
+    // Constructor for User with username and password parameters
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    // Getter method for retrieving the User's ID
     public Long getId() {
         return id;
     }
 
+    // Setter method for setting the User's ID
     public void setId(Long id) {
         this.id = id;
     }
 
+    // Getter method for retrieving the User's username
     public String getUsername() {
         return username;
     }
 
+    // Setter method for setting the User's username
     public void setUsername(String username) {
         this.username = username;
     }
 
+    // Getter method for retrieving the User's password
     public String getPassword() {
         return password;
     }
 
+    // Setter method for setting the User's password
     public void setPassword(String password) {
         this.password = password;
     }
 
-    //TODO:maybe override toString and equals methods
+    // TODO: Consider overriding toString and equals methods for better object representation and comparisons.
 }
