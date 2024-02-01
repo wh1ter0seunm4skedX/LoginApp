58eba105cf62fbea7bff6f4d5cf721e1a8301588
b7av3.t7av3ler <101125385+wh1ter0seunm4skedX@users.noreply.github.com>
2024-02-01 21:24:09 +0200

    Comments to UserRepository.java
    Added comments to the method for better understanding in the future.

--- src/main/java/com/b7av3/loginapp/repository/UserRepository.java
+++ src/main/java/com/b7av3/loginapp/repository/UserRepository.java
@@ -4,7 +4,10 @@ import com.b7av3.loginapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// This interface defines a UserRepository for managing User entities.
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Custom query method to find a user by username
     User findByUsername(String username);
 }
