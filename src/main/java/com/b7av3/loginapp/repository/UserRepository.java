b65bd2da90a79b591335202e99ec16fe2a3d9b2d
b7av3.t7av3ler <101125385+wh1ter0seunm4skedX@users.noreply.github.com>
2024-02-01 19:47:56 +0200

    Added UserRepository for handling user data
    In this commit, we've set up a UserRepository to help us manage user data efficiently. This repository lets us perform actions like saving and finding users in our app. It's a handy tool to keep our user-related operations organized.

---
+++ src/main/java/com/b7av3/loginapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.b7av3.loginapp.repository;
+
+import com.b7av3.loginapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
