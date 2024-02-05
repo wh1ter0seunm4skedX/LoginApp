a90b1bb8983170169b8255fa2886e2511e49f317
b7av3.t7av3ler <101125385+wh1ter0seunm4skedX@users.noreply.github.com>
2024-02-05 06:41:26 +0200

    Minor changes for register user (instead of saving one)

--- src/main/java/com/b7av3/loginapp/service/UserService.java
+++ src/main/java/com/b7av3/loginapp/service/UserService.java
@@ -3,11 +3,9 @@ package com.b7av3.loginapp.service;
 import com.b7av3.loginapp.model.User;
 import com.b7av3.loginapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-// This class provides services related to User entities.
 @Service
 public class UserService {
 
@@ -15,12 +13,13 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.passwordEncoder = new BCryptPasswordEncoder();
+        this.passwordEncoder = passwordEncoder;
     }
-    // Registers a new user by encoding their password and saving them in the repository.
-    public User registerNewUser(User user) {
+
+    // Saves a user to the repository after encoding their password.
+    public User saveUser(User user) {
         // Encode the password
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         // Save the user
