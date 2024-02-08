29b102ce9c49824ab5c8809ba53aad605149fe72
b7av3.t7av3ler <101125385+wh1ter0seunm4skedX@users.noreply.github.com>
2024-02-08 16:23:26 +0200

    remove the signup from login controller for better structure.

--- src/main/java/com/b7av3/loginapp/controller/LoginController.java
+++ src/main/java/com/b7av3/loginapp/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,18 +21,4 @@ public class LoginController {
         model.addAttribute("user", new User());
         return "login";
     }
-/*
-    @GetMapping("signup")
-    public String showRegistrationForm(Model model) {
-        model.addAttribute("user", new User());
-        return "signup";
-    }
-
-    @PostMapping("signup")
-    public String registerUser(User user) {
-        userService.registerNewUser(user); // Ensure this method handles password encoding
-        return "redirect:/login?success";
-    }
-
- */
 }
