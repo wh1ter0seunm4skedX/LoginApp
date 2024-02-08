e54f8e251a6dfff27354126d2b837a965e7505bf
b7av3.t7av3ler <101125385+wh1ter0seunm4skedX@users.noreply.github.com>
2024-02-08 10:58:28 +0200

    Ground-work for signup page

--- src/main/java/com/b7av3/loginapp/controller/LoginController.java
+++ src/main/java/com/b7av3/loginapp/controller/LoginController.java
@@ -1,16 +1,37 @@
 package com.b7av3.loginapp.controller;
 
+import com.b7av3.loginapp.model.User;
+import com.b7av3.loginapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/")
 public class LoginController {
 
-    // Return the name of the Thymeleaf template for the login page
-    @GetMapping
-    public String showLoginForm() {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("login")
+    public String showLoginForm(Model model) {
+        model.addAttribute("user", new User());
         return "login";
     }
+/*
+    @GetMapping("signup")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @PostMapping("signup")
+    public String registerUser(User user) {
+        userService.registerNewUser(user); // Ensure this method handles password encoding
+        return "redirect:/login?success";
+    }
+
+ */
 }
